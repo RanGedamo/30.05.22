89273847a311a755eeb4f558a6c847254e422807
RanGedamo <mcrangedamo@gmail.com>
2022-05-31 03:18:13 +0300

    ..

--- main.js
+++ main.js
@@ -1,7 +1,94 @@
+
+// console.log(document.getElementsByTagName("h1")[0].textContent);
+
+// document.getElementsByTagName("h2")[0].innerText="my dom app";
+
+// var spann =document.getElementsByTagName("span")[0]
+// var h3 =document.getElementsByTagName("h3")[0]
+// var h4 =document.getElementsByTagName("h4")[0]
+// var h5 =document.getElementsByTagName("h5")[0]
+
+// console.log(spann.innerText,h3.innerText,h4.innerText,h5.innerText)
+// console.log(h3);
+// console.log(h4.innerHTML)
+
+// var p = document.getElementsByTagName("p")[0]
+// var article = document.getElementsByTagName("article")[0]
+// var span = document.getElementsByTagName("span")[0]
+// var h6 = document.getElementsByTagName("h6")[0]
+
+// console.log(p,article,span,h6)
+
+// function printname(){
+//     var Name = prompt("name?")
+//     document.write("<h2>"+Name+"</h2>")
+// }
+// printname()
+
+
+
+// function printspan(){
+//     var coll = prompt("color?")
+//     var num = +prompt("how match?")
+//     for(var i = 0;i<num;i++){
+//         document.write("<span>"+"<br>"+coll+"<br>"+"</span>")
+//     }
+// }
+// printspan()
+
+
+
+// function printtext(){
+//     var text = prompt("text?")
+//     var tag = prompt("tag?")
+//     document.write("<"+tag+">"+text+"<"+tag+">")
+// }
+// printtext()
+
+
+
+
+
+// function printinp(){
+//     var inp = prompt("input?")
+//     document.write("<input type="+inp+">")
+// }
+// printinp()
+
+
+// function printclass(className,text1){
+//     for(var i = 0;i<3;i++){
+//     document.getElementsByClassName(className)[i].innerText=text1
+//     }
+// }
+// printclass("ran",prompt("text?..."))
+
+
+// function printp() {
+//     for (var i = 0; i < 6; i++) {
+//     document.getElementsByTagName("p")[i].innerHTML="<span>text</span>"
+//     }
+// }
+// printp()
+
+
+
 // function printname(){
 //     var print1 = prompt("name?");
 //     document.write("<h2>"+print1+"</h2>")
 // }
 // printname()
 
-function 
+
+// function userDate (){
+//     var username = prompt("enter name");
+//     var lastname = prompt("enter last name");
+//     var userage = +prompt('enter age');
+//     document.write(username+"<br>"+lastname+"<br>"+userage+"<br>")
+//     if (userage >18){
+//         document.write("<input type=text>")
+//     }
+// }
+// userDate()
+
+
